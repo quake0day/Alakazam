59a584a3c109fbde083ea4c85896ab835f690eed
quake0day <quake0day@gmail.com>
2013-04-09 01:35:49 -0400

    change

---
+++ src/fake.sh
@@ -0,0 +1,23 @@
+python insert_data.py 43.0024953299658 -78.78757774829865
+read -n1 -p "p"
+python insert_data.py 43.002512003256 -78.78753885626793  
+read -n1 -p "p"
+python insert_data.py 43.00254338826061 -78.78754019737244
+read -n1 -p "p"
+python insert_data.py 43.00257575402985 -78.78754287958145
+read -n1 -p "p"
+python insert_data.py 43.0026287161609 -78.7874986231327 
+read -n1 -p "p"
+python insert_data.py 43.00268952447725 -78.78752946853638
+read -n1 -p "p"
+python insert_data.py 43.00281898714389 -78.7875348329544 
+read -n1 -p "p"
+python insert_data.py 43.002854295096526 -78.78735914826393 
+read -n1 -p "p"
+python insert_data.py 43.002856747036944 -78.7876246869564
+read -n1 -p "p"
+python insert_data.py 43.0028557662608 -78.78770917654037 
+read -n1 -p "p"
+python insert_data.py 43.00288469915063 -78.78766223788261 
+
+
