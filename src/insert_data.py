ff615cfea0d9dde64f678b7b1fa83d02a97b9615
quake0day <quake0day@gmail.com>
2013-04-09 01:38:31 -0400

    change for show

--- src/insert_data.py
+++ src/insert_data.py
@@ -1,6 +1,8 @@
 import pymongo
 import datetime
 from pymongo import MongoClient
+from time import sleep
+import sys
 
 
 def init_Mongo():
@@ -14,9 +16,14 @@ db = init_Mongo()
 posts = db.posts
 
 # insert new data
-post1 = {"Name":"piers","lat":"43.00283408332881","lng":"-78.7872705197334","date":datetime.datetime.utcnow()}
+lat = sys.argv[1]
+lng = sys.argv[2]
+print lat,lng
+post1 = {"Name":"piers","lat":lat,"lng":lng,"date":datetime.datetime.utcnow()}
 
-post2 = {"Name":"piers","lat":"43.0028772374798","lng":"-78.78690305709836","date":datetime.datetime.utcnow()}
+#post2 = {"Name":"piers","lat":"43.0028772374798","lng":"-78.78690305709836","date":datetime.datetime.utcnow()}
 
+#post_id = posts.insert(post2)
+
+#sleep(5)
 post_id = posts.insert(post1)
-post_id = posts.insert(post2)
